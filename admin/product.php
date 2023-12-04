13b10fd980fe4917fafdc3de2919de7d2e594ede
queenieangelou <143998542+queenieangelou@users.noreply.github.com>
2023-12-04 23:30:02 +0800

    addded id in line

--- admin/product.php
+++ admin/product.php
@@ -1,18 +1,20 @@
-<?
+<?php
 // Product.php
 class Product {
+    public $id;
     public $name;
     public $price;
     public $quantity;
 
-    public function __construct($name, $price, $quantity) {
+    public function __construct($id, $name, $price, $quantity) {
+        $this->id = $id;
         $this->name = $name;
         $this->price = $price;
         $this->quantity = $quantity;
     }
 
     public function display() {
-        echo "Product: {$this->name}, Price: {$this->price}, Quantity: {$this->quantity}";
+        echo "Product ID: {$this->id}, Name: {$this->name}, Price: {$this->price}, Quantity: {$this->quantity}";
     }
 }
 ?>
