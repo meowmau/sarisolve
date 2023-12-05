823c9412653e2ee5d52f99926f98248052310b09
queenieangelou <143998542+queenieangelou@users.noreply.github.com>
2023-12-06 03:27:37 +0800

    modified the dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,10 +12,9 @@
         <a href="dashboard.php" class="logo"><i class="bx bx-scan"></i><span>SariSolve</span></a>
 
         <ul class="navbar">
-        <li><a href="../admin/productContent.php" class="products">Products</a></li>
-        <li><a href="../admin/salesContent.php" class="sales">Sales</a></li>
-        <li><a href="../admin/reportContent.php" class="report">Report</a></li>
-        <li><a href="../admin/userContent.php" class="user">User</a></li>
+            <li><a href="../admin/productContent.php" class="products">Products</a></li>
+            <li><a href="../admin/salesContent.php" class="sales">Sales</a></li>
+            <li><a href="../admin/reportContent.php" class="report">Report</a></li>
         </ul>
         <div class="main">
             <div class="user-dropdown">
@@ -24,94 +23,102 @@
                     <a href="../home/login.php">Logout</a>
                 </div>
             </div>
+        </div>
     </header>
 
     <main role="main" class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="mt-4">Welcome, [Username]!</h1>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="mt-4">Welcome, [Username]!</h1>
+                </div>
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Revenue Today</h5>
-                        <p class="card-text" id="revenueToday"></p>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title">Revenue Today</h3>
+                            <p class="card-text" id="revenueToday"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title">Sales Today</h3>
+                            <p class="card-text" id="salesCount"></p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Total Capital</h5>
-                        <p class="card-text" id="totalCapital"></p>
-                    </div>
-                </div>
-            </div>
-        </div>
 
-        <div class="row mt-4">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Recently Added Products</h5>
-                        <ul id="recentlyAddedProducts"></ul>
+            <div class="row mt-4">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title">Recently Added Products</h3>
+                            <ul id="recentlyAddedProducts"></ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Recent Sales</h5>
-                        <ul id="recentSales"></ul>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title">Recent Sales</h3>
+                            <ul id="recentSales"></ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
+
+<!-- Other HTML code remains unchanged -->
 
 <script>
     <?php
-        $inventory = new Inventory();
+    require_once 'connection.php'; // Include the connection class
+    require_once 'inventory.php';
+    $inventory = new Inventory();
 
-        $revenueToday = $inventory->getRevenueToday();
-        $totalCapital = $inventory->getTotalCapital();
-        $recentlyAddedProducts = $inventory->getRecentlyAddedProducts();
-        $recentSales = $inventory->getRecentSales();
+    // Fetch data from the database
+    $revenueToday = $inventory->getRevenueToday();
+    $salesCount = $inventory->getSalesTodayCount();
+    $recentlyAddedProducts = $inventory->getRecentlyAddedProducts();
+    $recentSales = $inventory->getRecentSales();
     ?>
 
-    console.log('Revenue Today:', "<?php echo $revenueToday; ?>");
-    console.log('Total Capital:', "<?php echo $totalCapital; ?>");
-    console.log('Recently Added Products:', <?php echo json_encode($recentlyAddedProducts); ?>);
-    console.log('Recent Sales:', <?php echo json_encode($recentSales); ?>);
+    // Display Revenue Today
+    var revenueTodayElement = document.getElementById('revenueToday');
+    revenueTodayElement.innerHTML = `
+        <p class="card-text"> <?php echo date('F j, Y'); ?>: $<?php echo number_format($inventory->getRevenueToday(), 2); ?></p>
+    `;
 
-    document.getElementById('revenueToday').innerText = "<?php echo $revenueToday; ?>";
-    document.getElementById('totalCapital').innerText = "<?php echo $totalCapital; ?>";
+    // Display Total Sales
+    var salesCountElement = document.getElementById('salesCount');
+    salesCountElement.innerHTML = `
+        <p class="card-text"> <?php echo date('F j, Y'); ?>: <?php echo $inventory->getSalesTodayCount(); ?> sales</p>
+    `;
 
-    var recentlyAddedProducts = <?php echo json_encode($recentlyAddedProducts); ?>;
-    var recentSales = <?php echo json_encode($recentSales); ?>;
-
-    console.log('recentlyAddedProducts:', recentlyAddedProducts);
-    console.log('recentSales:', recentSales);
-
+    // Display Recently Added Products
     var recentlyAddedProductsList = document.getElementById('recentlyAddedProducts');
-    recentlyAddedProducts.forEach(function(product) {
-        var li = document.createElement('li');
-        li.innerText = product;
-        recentlyAddedProductsList.appendChild(li);
-    });
+    recentlyAddedProductsList.innerHTML = `
+        <ul>
+            ${<?php echo json_encode($recentlyAddedProducts); ?>.map(product => `<li>${product.name}</li>`).join('')}
+        </ul>
+    `;
 
+    // Display Recent Sales
     var recentSalesList = document.getElementById('recentSales');
-    recentSales.forEach(function(sale) {
-        var li = document.createElement('li');
-        li.innerText = sale;
-        recentSalesList.appendChild(li);
-    });
-
+    recentSalesList.innerHTML = `
+        <ul>
+            ${<?php echo json_encode($recentSales); ?>.map(sale => `<li>${sale}</li>`).join('')}
+        </ul>
+    `;
 </script>
+
+<!-- Other HTML code remains unchanged -->
+
 </body>
 </html>
